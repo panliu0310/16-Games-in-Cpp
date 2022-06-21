d4785db0b886197c50a47928fa958858ef970980
panliu0310 <liuhoipan@gmail.com>
2022-06-21 23:26:16 +0800

    Environmental setting

--- 00_SFML_environmental_setting/00_SFML_environmental_setting/main.cpp
+++ 00_SFML_environmental_setting/00_SFML_environmental_setting/main.cpp
@@ -4,9 +4,37 @@
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
+using namespace sf;
 
 int main(){
-	std::cout << "Hello World";
-	system("pause");
+	
+	// Window
+	RenderWindow window(VideoMode(800, 600), "Game 1", Style::Titlebar | Style::Close);
+	Event ev;
+
+	// Game loop
+	while (window.isOpen()) {
+		// Event polling
+		while (window.pollEvent(ev)) {
+			switch (ev.type) {
+				case Event::Closed:
+					window.close();
+					break;
+				case Event::KeyPressed:
+					if (ev.key.code == Keyboard::Escape)
+						window.close();
+					break;
+			}
+		}
+		// Update
+
+		// Render
+		window.clear(Color::Blue); // Clear Old frame
+
+		// Draw your game
+
+		window.display(); // Tell app that window is done drawing
+	}
+
 	return 0;
 }
